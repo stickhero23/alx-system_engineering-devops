736fa5b887fc36b386482cf269145f66af013460
stickhero23 <stickhero23@gmail.com>
2023-03-03 18:52:50 +0000

    adding 9

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,11 +1,2 @@
 #!/usr/bin/env ruby
-ARGF.each do |line|
-  match = line.match(/\[from:([^\]]+)\] \[to:([^\]]+)\] \[flags:([^\]]+)\]/)
-  if match
-    sender = match[1]
-    receiver = match[2]
-    flags = match[3]
-    puts "#{sender},#{receiver},#{flags}"
-  end
-end
-
+puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(',')
