3f5a4bbdc8931effc7a41a61291e03308ea8fc68
Stickhero23 <francisnyamache296@gmail.com>
2023-04-28 23:15:26 +0000

    Adding task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
-# creating a file in /tmp
+# creates a file in /tmp
 
-file {'/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
+file { '/tmp/school':
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
 }
